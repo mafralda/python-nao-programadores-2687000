345b482dab361bc387e0e7f3029f931a93e8d5ec
Hugo Amorim <81188178+mafralda@users.noreply.github.com>
2024-08-07 02:12:52 +0000

    Exercicio resolvido 3

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -1,11 +1,13 @@
+nome = "Hugo"
 data_nascimento = '05-10-1976'
 idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print(nome*3)
 
 # Realize uma operação entre dados do tipo inteiro e float
+print("O competidor tinha " + str(altura - 1) + " metros de altura quando tinha 6 anos de idade")
